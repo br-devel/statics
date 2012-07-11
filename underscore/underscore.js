f0f8dba8bcbe4aa692b02a63eed702ca7d3d5501
Eduardo Turiño <eturino@gmail.com>
2012-07-11 23:48:55 +0200

    util

--- underscore/underscore.js
+++ underscore/underscore.js
@@ -1110,6 +1110,82 @@ _.mixin({
 		var win = window.open(mypage, myname, settings);
 
 		win.window.focus();
+	},
+
+	checkemail:function (emailStr, required) {
+		if (!required && emailStr == '') {
+			return true;
+		}
+		var checkTLD = 1;
+		var knownDomsPat = /^(com|net|org|cat|edu|int|mil|gov|biz|aero|name|coop|info|pro|museum)$/;
+		var emailPat = /^(.+)@(.+)$/;
+		var specialChars = "\\(\\)><@,;:\\\\\\\"\\.\\[\\]";
+		var validChars = "\[^\\s" + specialChars + "\]";
+		var quotedUser = "(\"[^\"]*\")";
+		var ipDomainPat = /^\[(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\]$/;
+		var atom = validChars + '+';
+		var word = "(" + atom + "|" + quotedUser + ")";
+		var userPat = new RegExp("^" + word + "(\\." + word + ")*$");
+		var domainPat = new RegExp("^" + atom + "(\\." + atom + ")*$");
+		var matchArray = emailStr.match(emailPat);
+
+		if (matchArray == null) {
+			alert('Email Incorrecto');
+			return false;
+		}
+		var user = matchArray[1];
+		var domain = matchArray[2];
+
+		for (i = 0; i < user.length; i++) {
+			if (user.charCodeAt(i) > 127) {
+				alert('Email Incorrecto');
+				return false;
+			}
+		}
+		for (i = 0; i < domain.length; i++) {
+			if (domain.charCodeAt(i) > 127) {
+				alert('Email Incorrecto');
+				return false;
+			}
+		}
+
+		if (user.match(userPat) == null) {
+			alert('Email Incorrecto');
+			return false;
+		}
+
+		var IPArray = domain.match(ipDomainPat);
+		if (IPArray != null) {
+			for (var i = 1; i <= 4; i++) {
+				if (IPArray[i] > 255) {
+					alert('Email Incorrecto');
+					return false;
+				}
+			}
+			return true;
+		}
+
+		var atomPat = new RegExp("^" + atom + "$");
+		var domArr = domain.split(".");
+		var len = domArr.length;
+		for (i = 0; i < len; i++) {
+			if (domArr[i].search(atomPat) == -1) {
+				alert('Email Incorrecto');
+				return false;
+			}
+		}
+
+		if (checkTLD && domArr[domArr.length - 1].length != 2 && domArr[domArr.length - 1].search(knownDomsPat) == -1) {
+			alert('Email Incorrecto');
+			return false;
+		}
+
+		if (len < 2) {
+			alert('Email Incorrecto');
+			return false;
+		}
+
+		return true;
 	}
 
 
