613fcf5069661ee263f326693bdb619a5dfb17a2
Eduardo Turiño <eturino@gmail.com>
2012-07-13 15:45:36 +0200

    equalheights

---
+++ jquery/jquery.equalheights.js
@@ -0,0 +1,36 @@
+
+/**
+ * Equal Heights Plugin
+ * Equalize the heights of elements. Great for columns or any elements
+ * that need to be the same size (floats, etc).
+ *
+ * Version 1.0
+ * Updated 12/10/2008
+ *
+ * Copyright (c) 2008 Rob Glazebrook (cssnewbie.com)
+ *
+ * Usage: $(object).equalHeights([minHeight], [maxHeight]);
+ *
+ * Example 1: $(".cols").equalHeights(); Sets all columns to the same height.
+ * Example 2: $(".cols").equalHeights(400); Sets all cols to at least 400px tall.
+ * Example 3: $(".cols").equalHeights(100,300); Cols are at least 100 but no more
+ * than 300 pixels tall. Elements with too much content will gain a scrollbar.
+ *
+ */
+
+(function($) {
+	$.fn.equalHeights = function(minHeight, maxHeight) {
+		minHeight = minHeight || 0; //eturino modification: optional javascript params
+		maxHeight = maxHeight || false; //eturino modification: optional javascript params
+		tallest = (minHeight) ? minHeight : 0;
+		this.each(function() {
+			if($(this).height() > tallest) {
+				tallest = $(this).height();
+			}
+		});
+		if((maxHeight) && tallest > maxHeight) tallest = maxHeight;
+		return this.each(function() {
+			$(this).height(tallest).css("overflow","auto");
+		});
+	}
+})(jQuery);
