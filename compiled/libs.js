6dded99cbdaa651643070fd5c66c122be1e58078
Eduardo Turiño <eturino@gmail.com>
2012-07-23 16:23:30 +0200

    faltaba toolbox

--- compiled/libs.js
+++ compiled/libs.js
@@ -1,4 +1,5 @@
 //@codekit-prepend "../underscore/underscore.js";
+//@codekit-prepend "../toolbox/toolbox.js";
 //@codekit-prepend "../jquery/jquery.json.js";
 //@codekit-prepend "../jquery/jquery.ba-bbq.js";
 //@codekit-prepend "../jquery/jquery.base64.js";
