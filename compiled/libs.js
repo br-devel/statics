9a84b7c463f62e883f3de0b99ac779d473aa6324
Eduardo Turiño <eturino@gmail.com>
2012-07-24 16:22:29 +0200

    precargas + jquery dentro del concat

--- compiled/libs.js
+++ compiled/libs.js
@@ -1,7 +1,82 @@
 //@codekit-prepend "../underscore/underscore.js";
 //@codekit-prepend "../toolbox/toolbox.js";
+//@codekit-prepend "../jquery/jquery.js";
 //@codekit-prepend "../jquery/jquery.json.js";
 //@codekit-prepend "../jquery/jquery.ba-bbq.js";
 //@codekit-prepend "../jquery/jquery.base64.js";
 //@codekit-prepend "../jquery/jquery.cookie.js";
 //@codekit-prepend "../jquery/jquery.selectboxes.js";
+
+var _BR = _.isObject(window['_BR']) ? window['_BR'] : {};
+
+_BR.changeMetricas = function () {
+	//change and execute Metricas
+	window._metricas = window._metricas || [];
+
+	for (var i in window._metricas) {
+		if (window._metricas.hasOwnProperty(i)) {
+			var x = window._metricas[i];
+			try {
+				$.ajax({url:x, cache:false});
+			} catch (exc) {
+				try {
+					console.log('error metricas ' + exc.message);
+				} catch (exc2) {
+					//
+				}
+
+			}
+		}
+	}
+
+	window._metricas.push = function (x) {
+		try {
+			$.ajax({url:x, cache:false});
+		} catch (exc) {
+			try {
+				console.log('error metricas ' + exc.message);
+			} catch (exc2) {
+				//
+			}
+		}
+		return Array.prototype.push.apply(this, arguments);
+	}
+
+}
+
+
+_BR.changePreloads = function () {
+	//change and execute Metricas
+	window._preloads = window._preloads || [];
+
+	for (var i in window._preloads) {
+		if (window._preloads.hasOwnProperty(i)) {
+			var x = window._preloads[i];
+			try {
+				$R.preload(x);
+			} catch (exc) {
+				try {
+					console.log('error preload ' + exc.message);
+				} catch (exc2) {
+					//
+				}
+
+			}
+		}
+	}
+
+	window._preloads.push = function (x) {
+		try {
+			$R.preload(x);
+		} catch (exc) {
+			try {
+				console.log('error preload ' + exc.message);
+			} catch (exc2) {
+				//
+			}
+		}
+		return Array.prototype.push.apply(this, arguments);
+	}
+
+}
+
