f9cc635b52b0d95ad1d9cc1108c6e2e29b74d185
Eduardo Turiño <eturino@gmail.com>
2013-03-04 22:37:49 +0100

    problema con ie8 y generación de errores

--- compiled/libs-init.js
+++ compiled/libs-init.js
@@ -1,6 +1,39 @@
 //@codekit-prepend "../labjs/LAB.js";
 //@codekit-prepend "../js/md5.js";
 
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+		"use strict";
+		if (this == null) {
+			throw new TypeError();
+		}
+		var t = Object(this);
+		var len = t.length >>> 0;
+		if (len === 0) {
+			return -1;
+		}
+		var n = 0;
+		if (arguments.length > 1) {
+			n = Number(arguments[1]);
+			if (n != n) { // shortcut for verifying if it's NaN
+				n = 0;
+			} else if (n != 0 && n != Infinity && n != -Infinity) {
+				n = (n > 0 || -1) * Math.floor(Math.abs(n));
+			}
+		}
+		if (n >= len) {
+			return -1;
+		}
+		var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+		for (; k < len; k++) {
+			if (k in t && t[k] === searchElement) {
+				return k;
+			}
+		}
+		return -1;
+	}
+}
+
 var _BR = (window['_BR'] === Object(window['_BR'])) ? window['_BR'] : {};
 
 _BR.getScriptPreloadeds = function () {
@@ -48,4 +81,4 @@ _BR.getScriptPreloadeds = function () {
 	} else {
 		return false;
 	}
-}
+};
